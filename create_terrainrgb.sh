889923976924a992f0d964b5679f0f305a96cbf3
acalcutt <acalcutt@techidiots.net>
2024-12-11 11:06:31 -0500

    change defaults

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -16,7 +16,7 @@ italy2=${OUTPUT_DIR}/italy_warp.vrt
 swiss1=${OUTPUT_DIR}/swiss.vrt
 swiss2=${OUTPUT_DIR}/swiss_warp.vrt
 outvrt=${OUTPUT_DIR}/out.vrt
-mbtiles=${OUTPUT_DIR}/jaxa_swiss_italy_terrainrgb_z0-Z12_webp_lanczos.mbtiles
+mbtiles=${OUTPUT_DIR}/jaxa_swiss_italy_terrainrgb_z5-Z14_webp_lanczos.mbtiles
 
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
@@ -43,17 +43,17 @@ printf '%s\n' ${swiss2} >>filenames.txt
 
 #Create terrain mbtiles 
 gdalbuildvrt -overwrite -resolution highest -r lanczos ${outvrt} -input_file_list filenames.txt
-rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 12 -j 24 --format webp ${outvrt} ${mbtiles}
+rio rgbify -b -10000 -i 0.1 --min-z 5 --max-z 14 -j 24 --format webp ${outvrt} ${mbtiles}
 
 sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
-sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_swiss_italy_terrainrgb_z0-Z12_webp" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "jaxa_swiss_italy_terrainrgb_z5-Z14_webp" WHERE name = "name" AND value = "";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA AW3D30 (2024), swissALTI3D (2024), and Tinitaly  (2023) converted with rio rgbify" WHERE name = "description";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "webp" WHERE name = "format";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
 sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=https://earth.jaxa.jp/en/data/policy/>JAXA AW3D30 (2024)</a> | <a href=https://www.swisstopo.admin.ch/en/height-model-swissalti3d>swissALTI3D (2024)</a>'); | <a href=https://tinitaly.pi.ingv.it/>Tinitaly (2023)</a>');"
-sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('minzoom','0');"
-sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('maxzoom','12');"
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('minzoom','5');"
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('maxzoom','14');"
 sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('bounds','-180,-90,180,90');"
-sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('center','0,0,5');"
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('center','0,0,10');"
 
