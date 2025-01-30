8f7fd66ca56b8e16e976ddc149d970c23aed0267
acalcutt <acalcutt@techidiots.net>
2025-01-29 19:50:03 -0500

    rename folder

--- SonnyDem_EUROPE/create_terrainrgb.sh
+++ SonnyDem_EUROPE/create_terrainrgb.sh
@@ -12,7 +12,7 @@ OUTPUT_DIR=./output
 [[ $FORMAT ]] || FORMAT=webp
 [[ $RESAMPLING ]] || RESAMPLING=cubic
 
-BASENAME=SONNY_DEM_2024_TerrainRGB_z${MINZOOM}-Z${MAXZOOM}_${RESAMPLING}_${FORMAT}
+BASENAME=SONNY_DEM_2024_Europe_TerrainRGB_z${MINZOOM}-Z${MAXZOOM}_${RESAMPLING}_${FORMAT}
 vrtfile=${OUTPUT_DIR}/${BASENAME}.vrt
 mbtiles=${OUTPUT_DIR}/${BASENAME}.mbtiles
 vrtfile2=${OUTPUT_DIR}/${BASENAME}_warp.vrt
@@ -21,6 +21,4 @@ vrtfile2=${OUTPUT_DIR}/${BASENAME}_warp.vrt
 
 gdalbuildvrt -overwrite -resolution highest -r $RESAMPLING -srcnodata -9999 -vrtnodata -9999 ${vrtfile} ${INPUT_DIR}/*.hgt
 gdalwarp -r $RESAMPLING -t_srs EPSG:3857 -dstnodata 0 ${vrtfile} ${vrtfile2}
-rio rgbify -v -e terrarium --min-z $MINZOOM --max-z $MAXZOOM -j $THREADS --batch-size $BATCH --resampling $RESAMPLING --format $FORMAT ${vrtfile2} ${mbtiles}
-
-
+rio rgbify -v -b -10000 -i 0.1 --min-z $MINZOOM --max-z $MAXZOOM -j $THREADS --batch-size $BATCH --resampling $RESAMPLING --format $FORMAT ${vrtfile2} ${mbtiles}
---
+++ SonnyDem_EUROPE/download/README.md
@@ -0,0 +1,4 @@
+Download DTM 1" EUROPE zip files from https://sonny.4lima.de/ and place them in this directory
+
+The current download link is https://bit.ly/dtm-europe-1s-v2
+
