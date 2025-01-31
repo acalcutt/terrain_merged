a1330087172d489da70dc838ac336c23641723a9
acalcutt <acalcutt@techidiots.net>
2025-01-30 23:34:26 -0500

    update gebco scripts

--- GEBCO/create_terrainrgb.sh
+++ GEBCO/create_terrainrgb.sh
@@ -6,20 +6,22 @@ INPUT_DIR=./gebco
 OUTPUT_DIR=./output
 
 [[ $THREADS ]] || THREADS=16
-[[ $BATCH ]] || BATCH=25
+[[ $BATCH ]] || BATCH=32
 [[ $MINZOOM ]] || MINZOOM=0
 [[ $MAXZOOM ]] || MAXZOOM=8
 [[ $FORMAT ]] || FORMAT=webp
-[[ $RESAMPLING ]] || RESAMPLING=lanczos
+[[ $RESAMPLING ]] || RESAMPLING=cubic
+[[ $BASE_VALUE ]] || BASE_VALUE=-10000
+[[ $INTERVAL ]] || INTERVAL=0.1
 
-BASENAME=GEBCO_2024_TerrainRGB_${MINZOOM}-${MAXZOOM}_${FORMAT}
+BASENAME=GEBCO_2024_TerrainRGB_${MINZOOM}-${MAXZOOM}_${RESAMPLING}_${FORMAT}
 vrtfile=${OUTPUT_DIR}/${BASENAME}.vrt
 vrtfile2=${OUTPUT_DIR}/${BASENAME}_warp.vrt
 mbtiles=${OUTPUT_DIR}/${BASENAME}.mbtiles
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
-gdalbuildvrt -overwrite -resolution highest -r lanczos ${vrtfile} ${INPUT_DIR}/*.tif
-gdalwarp -r lanczos -s_srs epsg:4326 -t_srs EPSG:3857 -dstnodata 0 ${vrtfile} ${vrtfile2}
-rio rgbify -v -b -10000 -i 0.1 --min-z $MINZOOM --max-z $MAXZOOM -j $THREADS --batch-size $BATCH --resampling $RESAMPLING --format $FORMAT  ${vrtfile2} ${mbtiles}
+gdalbuildvrt -overwrite -resolution highest -r "$RESAMPLING" ${vrtfile} ${INPUT_DIR}/*.tif
+gdalwarp -r "$RESAMPLING" -s_srs epsg:4326 -t_srs EPSG:3857 -dstnodata "$BASE_VALUE" ${vrtfile} ${vrtfile2}
+rio rgbify -v -b "$BASE_VALUE" -i "$INTERVAL" --min-z "$MINZOOM" --max-z "$MAXZOOM" -j "$THREADS" --batch-size "$BATCH" --resampling "$RESAMPLING" --format "$FORMAT" ${vrtfile2} ${mbtiles}
 
