da08a15db978a0ccc7c75cbf7b20c85ac42d2014
Andrew Calcutt <acalcutt@techidiots.net>
2026-03-14 13:38:26 -0400

    Update download script for GEBCO data to 2025

--- datasets/GEBCO/download_extract.sh
+++ datasets/GEBCO/download_extract.sh
@@ -3,8 +3,8 @@
 INPUT_DIR=./gebco
 [ -d "$INPUT_DIR" ] || mkdir -p $INPUT_DIR || { echo "error: $INPUT_DIR " 1>&2; exit 1; }
 cd $INPUT_DIR
-wget -O gebco_2024_sub_ice_topo_geotiff.zip https://www.bodc.ac.uk/data/open_download/gebco/gebco_2024_sub_ice_topo/geotiff/
+wget -O gebco_2025_sub_ice_topo_geotiff.zip https://dap.ceda.ac.uk/bodc/gebco/global/gebco_2025/sub_ice_topography_bathymetry/geotiff/
 
-unzip gebco_2024_sub_ice_topo_geotiff.zip
+unzip gebco_2025_sub_ice_topo_geotiff.zip
 cd ..
 
